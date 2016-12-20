580acef5a3c7bf80c4b9aef26ae56a7784181238
pasaunders <kaorti@gmail.com>
2016-12-19 17:20:12 -0800

    step1 website ready.

--- learning-journal-basic/learning_journal_basic/routes.py
+++ learning-journal-basic/learning_journal_basic/routes.py
@@ -4,3 +4,6 @@ def includeme(config):
     """This function adds routes to pyramid's configurator."""
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('new', '/journal/new-entry')
+    config.add_route('detail', '/journal/1')
+    config.add_route('edit', '/journal/1/edit-entry')
--- learning-journal-basic/learning_journal_basic/views.py
+++ learning-journal-basic/learning_journal_basic/views.py
@@ -1,9 +1,31 @@
 from pyramid.response import Response
+import os
+
+HERE = os.path.dirname(__file__)
 
 
 def home_page(request):
-    return Response("This is my first view.")
+    imported_text = open(os.path.join(HERE, 'templates/main.html')).read()
+    return Response(imported_text)
+
+
+def new_entry(request):
+    imported_text = open(os.path.join(HERE, 'templates/new.html')).read()
+    return Response(imported_text)
+
+
+def detail_page(request):
+    imported_text = open(os.path.join(HERE, 'templates/detail.html')).read()
+    return Response(imported_text)
+
+
+def edit_page(request):
+    imported_text = open(os.path.join(HERE, 'templates/edit.html')).read()
+    return Response(imported_text)
 
 
 def includeme(config):
     config.add_view(home_page, route_name='home')
+    config.add_view(new_entry, route_name='new')
+    config.add_view(detail_page, route_name='detail')
+    config.add_view(edit_page, route_name='edit')
