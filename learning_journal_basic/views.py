56fc5212134c914c40e8c8f38412e233e23df8e2
pasaunders <kaorti@gmail.com>
2016-12-20 19:26:28 -0800

    working main and detail pages

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -11,7 +11,7 @@ HERE = os.path.dirname(__file__)
 ENTRIES = [
     {"title": "Day 11",
      "id": 1,
-     "creation_date": """Today we learnt so many new things! First we
+     "body": """Today we learnt so many new things! First we
      learned about web frameworks and in particular how to use Pyramid.
      We went pretty quickly over this so I got a little lost in class but
      doing it twice for both me and my partner's learning journal was great
@@ -24,7 +24,7 @@ ENTRIES = [
     my head around how they work and what they could be used for.
     Today was a deque which was similar to a queue and a double
     linked list..""",
-     "body": "I learned"},
+     "creation_date": "Dec 19, 2016"},
 
     {"title": "Day 12",
      "id": 2,
@@ -36,11 +36,7 @@ ENTRIES = [
 @view_config(route_name="home", renderer="templates/list.jinja2")
 def home_page(request):
     """View the home page."""
-    all_my_stuff = [
-        'laptop',
-        'lunch'
-    ]
-    return {"bag_list": all_my_stuff}
+    return {"entries": ENTRIES}
 
 
 # @view_config(route_name="home", renderer="string")
@@ -51,33 +47,34 @@ def home_page(request):
 #     return file_data
 
 
-@view_config(route_name="new", renderer="string")
-def new_entry(request):
-    """View the new entry page."""
-    imported_text = open(os.path.join(HERE, 'templates/new.html')).read()
-    # return Response(imported_text)
-    return imported_text
+# @view_config(route_name="new", renderer="string")
+# def new_entry(request):
+#     """View the new entry page."""
+#     imported_text = open(os.path.join(HERE, 'templates/new.html')).read()
+#     # return Response(imported_text)
+#     return imported_text
 
 
-@view_config(route_name="detail", renderer="string")
+@view_config(route_name="detail", renderer="templates/detail.jinja2")
 def detail_page(request):
     """View the detail page."""
-    imported_text = open(os.path.join(HERE, 'templates/detail.html')).read()
-    # return Response(imported_text)
-    return imported_text
+    entry_id = request.matchdict['id']
+    dictionary_filtered = [entry for entry in ENTRIES if entry['id'] == int(entry_id)]
+    # import pdb; pdb.set_trace()
+    return {"entries": dictionary_filtered[0]}
 
 
-@view_config(route_name="edit", renderer="string")
-def edit_page(request):
-    """View the edit page."""
-    imported_text = open(os.path.join(HERE, 'templates/edit.html')).read()
-    # return Response(imported_text)
-    return imported_text
+# @view_config(route_name="edit", renderer="string")
+# def edit_page(request):
+#     """View the edit page."""
+#     imported_text = open(os.path.join(HERE, 'templates/edit.html')).read()
+#     # return Response(imported_text)
+#     return imported_text
 
 
-# def includeme(config):
-#     """Configure the journal pages for viewing."""
-#     config.add_view(home_page, route_name='home')
-#     config.add_view(new_entry, route_name='new')
-#     config.add_view(detail_page, route_name='detail')
-#     config.add_view(edit_page, route_name='edit')
+# # def includeme(config):
+# #     """Configure the journal pages for viewing."""
+# #     config.add_view(home_page, route_name='home')
+# #     config.add_view(new_entry, route_name='new')
+# #     config.add_view(detail_page, route_name='detail')
+# #     config.add_view(edit_page, route_name='edit')
