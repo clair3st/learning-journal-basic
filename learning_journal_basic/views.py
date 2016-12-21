ca98d18ad7dbf2fecfb7ba47874d5725baf9c990
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-20 22:26:21 -0800

    added views for new and edit

--- learning_journal_basic/views.py
+++ learning_journal_basic/views.py
@@ -19,16 +19,23 @@ ENTRIES = [
      frustration I realized t was a problem with my file tree setup.
      Luckily I was able to get my site up and running.<br>
             I'm really enjoying the data structures at the moment.
-    I feel like the more we do the easier it is for me to get
-    my head around how they work and what they could be used for.
-    Today was a deque which was similar to a queue and a double
-    linked list..""",
+     I feel like the more we do the easier it is for me to get
+     my head around how they work and what they could be used for.
+     Today was a deque which was similar to a queue and a double
+     linked list.""",
      "creation_date": "Dec 19, 2016"},
 
     {"title": "Day 12",
      "id": 2,
      "creation_date": "Dec 20, 2016",
-     "body": "I learned about "},
+     "body": """I learned about the binary heap data structure. I can see that
+     this would shorten the time needed to sort data compated to just looping
+     through a normal list. I found it hard to get my head around not using
+     nodes when we implemented it using a list in python.<br>
+     We also learned about jinja2 today and using it as a templating system for
+     a website. I think initially I was a little freaked out by jinja when I
+     did the reading but now that I've had the chance to practise and implement
+     it in this site I feel like I'm starting to get the hang of it. """},
 ]
 
 
